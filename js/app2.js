69a125fca7c6ac7f3ff070902335b9c48eb410e8
Alexander <aporcal13@gmail.com>
2025-11-17 17:27:17 -0300

    casi ultimos cambios añadidos

--- js/app2.js
+++ js/app2.js
@@ -1,70 +1,14 @@
-const products = [
-  {
-    id: 1,
-    name: "Leche",
-    price: 56,
-    description: "Leche entera de 1 litro, ideal para consumo diario",
-  },
-  {
-    id: 2,
-    name: "Pan de molde",
-    price: 75,
-    description: "Pan blanco suave, ideal para sándwiches",
-  },
-  {
-    id: 3,
-    name: "Café instantáneo",
-    price: 220,
-    description: "Café soluble de sabor intenso",
-  },
-  {
-    id: 4,
-    name: "Arroz blanco",
-    price: 110,
-    description: "Arroz tipo largo fino, paquete de 1 kg",
-  },
-  {
-    id: 5,
-    name: "Aceite de girasol",
-    price: 180,
-    description: "Aceite vegetal de 1 litro, apto para cocinar o freír",
-  },
-  {
-    id: 6,
-    name: "Fideos spaghetti",
-    price: 95,
-    description: "Pasta tipo spaghetti de trigo duro",
-  },
-  {
-    id: 7,
-    name: "Azúcar refinada",
-    price: 130,
-    description: "Azúcar blanca común, paquete de 1 kg",
-  },
-  {
-    id: 8,
-    name: "Yerba mate",
-    price: 250,
-    description: "Yerba tradicional con palo, paquete de 1 kg",
-  },
-  {
-    id: 9,
-    name: "Manteca",
-    price: 140,
-    description: "Manteca natural de 200 gramos",
-  },
-  {
-    id: 10,
-    name: "Jabón de tocador",
-    price: 60,
-    description: "Jabón humectante con aroma floral",
-  },
-];
 
-let carrito = [];
+
+fetch("../products.json")
+  .then(response => response.json())
+  .then(products => {
+    console.log("Productos cargados:", products);
+      
+    let carrito = [];
 
 let productsShown = document.querySelector("#prods-sct");
-let productsShown2 = document.querySelector("#prods2-sct");
+
 
 products.forEach((prod) => {
   productsShown.innerHTML += `<div class="card m-3 flex-wrap card shadow p-3" style="width: 18rem;">
@@ -75,14 +19,16 @@ products.forEach((prod) => {
           data-id="${prod.id}" 
           data-name="${prod.name}"
           data-price="${prod.price}"
-          data-description="${prod.description}" 
+          data-description="${prod.description}"
+          data-quantity= "${prod.quantity}"
           type="button" 
           class="btn btn-dark  btn-buy"> 
             Agregar al carrito
     </button>
     </div>
     </div>`;
-});
+
+    
 
 let buttons = document.querySelectorAll(".btn-buy");
 
@@ -95,13 +41,17 @@ buttons.forEach((btn) => {
       name: btn.dataset.name,
       price: parseFloat(btn.dataset.price),
       description: btn.dataset.description,
+      quantity: btn.dataset.quantity
     };
 
-    if (product) {
+
       carrito.push(product);
 
       localStorage.setItem("carrito", JSON.stringify(carrito));
-    }
+  });
+});
+
+
   });
 });
 
--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,82 @@
-const stringLocal = localStorage.getItem("carrito");
-console.log(stringLocal);
+//Unifico productos iguales
+function unificarCarrito() {
+  const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  const carritoUnificado = [];
+
+  carrito.forEach((producto) => {
+    const exist = carritoUnificado.find(p => p.id === producto.id);
+
+    if (exist) {
+      exist.quantity = Number(exist.quantity) + Number(producto.quantity);
+    } else {
+      carritoUnificado.push({ ...producto });
+    }
+  });
+
+  localStorage.setItem("carrito", JSON.stringify(carritoUnificado));
+
+  return carritoUnificado;
+}
+
+unificarCarrito();
+console.log(unificarCarrito());
+
+
+
+
+//ubico la seccion y creo la seccion del carrito con los productos del storage
 const cartLocalStorage = JSON.parse(localStorage.getItem("carrito")) || [];
 console.log(cartLocalStorage);
-
 const prodsDeCarrito = document.getElementById("prodsDeCarrito");
 
 cartLocalStorage.forEach((prod) => {
+  let total = Number(prod.price) * Number(prod.quantity)
   prodsDeCarrito.innerHTML += `
-    <div class="container d-flex justify-content-center mt-5">
+    <div class="container d-flex justify-content-center mt-5 mb-3">
   <div class="card shadow p-3" style="width: 26rem;">
     <div class="d-flex">
       <div>
         <h5>${prod.name}</h5>
-        <p class="mb-2">Cantidad</p>
-        <span class="fw-bold">$${prod.price} </span>
+        <p class="mb-2">Cantidad ${prod.quantity} </p>
+        <span class="fw-bold">$${total} </span>
       </div>
     </div>
 
     <hr>
 
     <div class="d-flex justify-content-between">
-      <button id="delete${prod.id}class="btn btn-danger">Eliminar</button>
-      <button class="btn btn-success">Comprar articulo</button>
+      <button id="delete${prod.id}" class="btn btn-danger" onclick="eliminarDelCarrito(${prod.id})">Eliminar</button>
     </div>
   </div>
 </div>
     `;
 });
+
+// elimina del carrito el producto con el boton
+function eliminarDelCarrito(id) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  console.log(carrito);
+
+  let updatedCarrito = carrito.filter(product => product.id != id);
+
+  localStorage.setItem("carrito", JSON.stringify(updatedCarrito));
+
+  location.reload();
+  
+}
+
+//Obtengo compras y lo incluyo al campo del total recargando pagina y limpiando local storage
+const total = cartLocalStorage.reduce((acc, prod) => acc + prod.price * prod.quantity, 0);
+
+document.getElementById("totalCompra").textContent = `$${total}`;
+
+document.getElementById("confirmarBtn").addEventListener("click", () => {
+  alert("Compra confirmada!");
+
+  localStorage.removeItem("carrito");
+
+  location.reload();
+  });
+
